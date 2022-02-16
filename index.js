2fbcb63c500f7400db169c242993bd1a9145083c
andreespirela <andreespirela@outlook.com>
2022-02-16 11:31:48 -0500

    Adding dispatcher job

--- index.js
+++ index.js
@@ -11,26 +11,10 @@ import { sendNews } from "./send-news.js";
     await config();
     await generateWallet();
     
-    const expressApp = express();
-    expressApp.post('/', async (req, res) => {
-        if(process.env.REQ_SECRET !== req.query.secret) {
-            res.sendStatus(403);
-        } else {
-            try {
-                const news = await newsApi.request("ukraine") || {};
-                const russia = await newsApi.request("russia") || {};
-                const all = [...(news.articles || []), ...(russia.articles || [])];
-                res.send(await sendNews(all));
-            } catch(e) {
-                res.sendStatus(500);
-            }
-        }
-    });
-
-    const port = process.env.PORT || 3000;
-    expressApp.listen(port, async () => {
-        console.log(`Example app listening on port ${port}`);
-        console.log(`Public key: ${await arweave.wallets.jwkToAddress(getWallet())}`);
-    });
-
+    console.log(`Public key: ${await arweave.wallets.jwkToAddress(getWallet())}`);
+    
+    const news = await newsApi.request("ukraine") || {};
+    const russia = await newsApi.request("russia") || {};
+    const all = [...(news.articles || []), ...(russia.articles || [])];
+    console.log(await sendNews(all));
 })();
--- wallet.js
+++ wallet.js
@@ -1,7 +1,26 @@
 import { writeFileSync, readFileSync } from 'fs';
 import { fileExists, arweave } from './utils.js';
+import { Buffer as BufferImp } from 'buffer';
+
+global.Buffer = global.Buffer || BufferImp;
+
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str, 'binary').toString('base64');
+  };
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  };
+}
 
 export const generateWallet = async () => {
+    if(process.env.WALLET) {
+        console.log("Env wallet found");
+        return;
+    }
     if (!fileExists('./wallet.json')) {
         writeFileSync('wallet.json', JSON.stringify(await arweave.wallets.generate()));
         console.log("Generated wallet");
@@ -11,6 +30,12 @@ export const generateWallet = async () => {
 };
 
 export const getWallet = () =>  {
+
+    if(process.env.WALLET) {
+        console.log("Env wallet found");
+        return JSON.parse(atob(process.env.WALLET));
+    }
+
     if (fileExists('./wallet.json')) {
         return JSON.parse(readFileSync('wallet.json', 'utf8'));
     } else {
